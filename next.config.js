cb4c49b58dc37df52706a4931c0bef94cabdedd6
droussiaymane <aymanedroussi.off@gmail.com>
2023-08-05 13:41:15 +0200

    fix

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const baseConfig = require('./next.config.base');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   ...baseConfig,
+  env: {
+    NEXTAUTH_URL: "http://68.183.29.239:3000",
+  },
 };
 
 module.exports = nextConfig;
