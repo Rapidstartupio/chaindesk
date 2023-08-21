4015f141be745aa2b8630fd9a4c315d45d6f917e
droussiaymane <aymanedroussi.off@gmail.com>
2023-08-21 21:47:19 +0200

    add admin role

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -76,21 +76,66 @@ export const authOptions = {
   ],
   callbacks: {
     async session({ session, user, token }) {
-      return {
-        ...session,
-        user: {
-          ...session.user,
-          usage: (user as any)?.usage as Usage,
-          nbAgents: (user as any)?.['_count']?.agents as number,
-          nbDatastores: (user as any)?.['_count']?.datastores as number,
-          id: user.id,
-          currentPlan:
-            (user as any)?.subscriptions?.[0]?.plan ||
-            (SubscriptionPlan.level_0 as SubscriptionPlan),
-          isPremium: (user as any)?.subscriptions?.length > 0,
-          customerId: (user as any)?.subscriptions?.[0]?.customerId as string,
-        },
-      };
+      // check if user role ==admin
+      if ((user as any)?.role === 'admin') {
+        return {
+          ...session,
+          user: {
+            ...session.user,
+
+            /*!SECTION
+            usage: {
+            "id": "cllktladl0001pr7tz8idoqtu",
+            "userId": "cllktladl0000pr7t571y5g25",
+            "nbAgentQueries": 5,
+            "nbDatastoreQueries": 0,
+            "nbUploadedBytes": 0,
+            "nbDataProcessingBytes": 321,
+            "nbModelTokens": 0
+        }
+
+        create this usage
+        */
+        usage:
+        {
+          "id": "ADMIN_USAGE",
+          "userId": user.id,
+          "nbAgentQueries": 1000000,
+          "nbDatastoreQueries": 1000000,
+          "nbUploadedBytes": 1000000,
+          "nbDataProcessingBytes": 1000000,
+          "nbModelTokens": 1000000
+        }
+            ,
+            nbAgents: 10000000,
+            nbDatastores: 10000000,
+            id: user.id,
+            currentPlan:"level_3",
+            isPremium: true,
+            customerId: "ADMIN",
+          },
+        };
+      }
+      else{
+        return {
+          ...session,
+          user: {
+            ...session.user,
+            usage: (user as any)?.usage as Usage,
+            nbAgents: (user as any)?.['_count']?.agents as number,
+            nbDatastores: (user as any)?.['_count']?.datastores as number,
+            id: user.id,
+            currentPlan:
+              (user as any)?.subscriptions?.[0]?.plan ||
+              (SubscriptionPlan.level_0 as SubscriptionPlan),
+            isPremium: (user as any)?.subscriptions?.length > 0,
+            customerId: (user as any)?.subscriptions?.[0]?.customerId as string,
+          },
+        };
+      }
+
+
+
     },
   },
   redirect: {
