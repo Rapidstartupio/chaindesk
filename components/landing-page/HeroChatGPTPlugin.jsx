bbf9856ca9d56626f43b3468fde5a164962292bf
droussiaymane <aymanedroussi.off@gmail.com>
2023-08-25 19:40:53 +0200

    fix api issues and 404 issue

--- components/landing-page/HeroChatGPTPlugin.jsx
+++ components/landing-page/HeroChatGPTPlugin.jsx
@@ -247,7 +247,7 @@ export function HeroChatGPTPlugin() {
             <div className='w-full h-[500px] overflow-hidden rounded-3xl bg-white'>
 
             <iframe
-              src="https://widget.rapidagent.ai/agents/clgtujkqh022j0u0zw3ut8vk3/iframe"
+              src="https://widget.rapidagent.ai/agents/cllqw2lvo0001bzofgvph19nv/iframe"
               width="100%"
               height="100%"
               frameBorder="0"
